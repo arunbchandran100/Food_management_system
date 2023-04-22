91c2f390b5d1dc15e062371ae7ae57ab3004b43a
deepak368 <77375511+deepak368@users.noreply.github.com>
2023-04-23 04:35:19 +0530

    deepak first commit

--- Food_management_system/doner/urls.py
+++ Food_management_system/doner/urls.py
@@ -1,15 +1,12 @@
 
 from django.urls import path
 from .import views
-from .views import DonateFoodView,DonationHistoryView,FeedbackView,FeedbacklistView,DonationEditView,DonationDeleteView
+
 
 
 urlpatterns = [
     path('index',views.home,name="index"),
-    path('donate-food',DonateFoodView.as_view(),name='donate-food'),
-    path('donation-history',DonationHistoryView.as_view(),name='donation-history'),
-    path('feedback',FeedbackView.as_view(),name="feedback"),
-    path('feedback-list',FeedbacklistView.as_view(),name="feedback-list"),
-    path('donation-edit/<int:pk>',DonationEditView.as_view(),name="donation-edit"),
-    path('donation-delete/<int:pk>',DonationDeleteView.as_view(),name='donation-delete')
+    path('donate_food',views.donate_food,name='donate-food'),
+
+    
 ]
