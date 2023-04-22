157c3a59b54e2209f68c130ee5af5aaa04d4b81a
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2023-04-22 10:36:33 +0530

    working

--- Food_management_system/website/models.py
+++ Food_management_system/website/models.py
@@ -8,10 +8,13 @@ from django.utils.translation import gettext as _
 from django.conf import settings
 
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
     forget_password_token = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
 
     def __str__(self):
         return self.user.username
