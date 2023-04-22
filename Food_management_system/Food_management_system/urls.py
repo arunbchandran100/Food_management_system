ad85ec0294311bdc8062a1f3167b4e78a7ef32de
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2023-04-22 21:50:52 +0530

    created urls.py in website app
    updated the urls.py path in main app(food management system) also.

--- Food_management_system/Food_management_system/urls.py
+++ Food_management_system/Food_management_system/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from website import views
 #from .views import UserEditView
 from django.contrib.auth import views as auth_views 
@@ -8,13 +8,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.SignupPage,name='signup'),
-    path('login/',views.LoginPage,name='login'),
-    path('home/',views.HomePage,name='home'),
-    path('logout/',views.LogoutPage,name='logout'),
-    
-    path('vprofile/',views.profile,name='vprofile'),
-    
-    path('forget_password/' ,views.ForgetPassword , name="forget_password"),
-    path('change_password/<token>/' , views.ChangePassword , name="change_password"),
+    path('website/', include('website.urls')),
+
 ]
