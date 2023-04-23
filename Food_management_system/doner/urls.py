916f644ee650f1a9ee757c6b3c011eeb7baf2ec8
deepak368 <77375511+deepak368@users.noreply.github.com>
2023-04-23 21:58:09 +0530

    donation history created

--- Food_management_system/doner/urls.py
+++ Food_management_system/doner/urls.py
@@ -7,6 +7,8 @@ from .import views
 urlpatterns = [
     path('index',views.home,name="index"),
     path('donate_food',views.donate_food,name='donate-food'),
+    path('donation_history',views.donation_history,name='donation-history'),
+
 
     
 ]
